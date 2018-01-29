4c85b2b6898e95038e102538a2dcf4221198f888
EMANJAAN <emanjaan89@gmail.com>
2018-01-28 22:26:33 -0500

    initial commit

---
+++ resources/views/welcome.blade.php
@@ -0,0 +1,112 @@
+<!doctype html>
+<html>
+    <head>
+      <meta name="viewport" content="width=device-width, initial-scale=1">
+      <meta http-equiv="X-UA-Compatible" content="IE-edge" />
+        <title>Emails</title>
+        <!-- Styles -->
+        <style type="text/css">
+        body{
+          padding: 0;
+          margin: 0;
+          font-family: sans-serif;
+        }
+            @@media screen and (max-width: 600px)
+            {
+              table[class="responsive-table"]
+              {
+                width: 100% !important;
+                padding: 10px 5%;
+              }
+
+              table[class="responsive-table2"]
+              {
+                width: 100% !important;
+
+              }
+
+              img[class="responsive-img"]
+              {
+                max-width: 100% !important;
+                height: auto !important;
+
+              }
+
+              td[class="headlines"]
+              {
+                font-size: 38px !important;
+                background: #222222;
+                color: #ffffff;
+                padding-bottom: 20px;
+              }
+
+            }
+
+        </style>
+    </head>
+    <body>
+
+      <!-- FULL WIDTH CONTAINER -->
+      <!--Outter table-->
+      <table width="100%" cellspaceing="0" cellpadding="0" border="0">
+        <tr>
+          <td align="center">
+
+            <!-- 600px CONTAINER -->
+            <!--Inner table 1-->
+            <table width="600" cellspaceing="0" cellpadding="0" border="0" class="responsive-table"> <!-- to take way the gaps in the email -->
+              <tr>
+                <td>
+                   <img src="http://davidlloyd.org/wp-content/uploads/2015/10/hello_1x.png" width="600" height="300" class="responsive-img">
+                 </td>
+              </tr>
+
+              <tr>
+                <td align="center" style="font-size:48px; padding-top: 28px;" class="headlines">
+                    I &hearts; pictures
+                </td>
+              </tr>
+
+              <tr>
+                 <td style="padding: 20px 0; font-size=20ps;">
+                   Converting an idea into a reality (regardless of the required investment of time and money) is never an easy task. In fact, it is extremely difficult. Whether you are an entrepreneur or corporate executive, “giving ideas life” is much like giving birth to a child. You must own the responsibility regardless of the circumstances. No one will ever understand your idea or the dynamics associated with it like you do. In this regard, you are on your own and the journey will require you to learn about yourself – more than anything else will in your career.
+
+As the old saying goes, “If it were easy – everyone would do it.”
+                 </td>
+              </tr>
+              <tr>
+                <td>
+
+                  <!--Inner table 2-->
+                  <table width="300" align="right" class="responsive-table2">
+                    <tr>
+                      <td bgcolor="#aaaaaa" cellpadding="0" cellspaceing="0" border="0" padding="15px">
+                        Believe in Yourself, Create Your Own Personal Board of Advisors, Embrace Risk as Your Best Friend, Be Extremely Patient
+                        Learn How to Sell Your Vision, Compromise is a choice, not a sacrifice.  Don’t put too much pressure on yourself.
+                        Take the time to appreciate the journey and understand how things work. Well, it is – not just to yourself, but to those around you.
+                  </table>
+
+                  <!--Inner table 3-->
+                  <table width="300" align="left" class="responsive-table2">
+                    <tr>
+                      <td bgcolor="#dddddd" cellspaceing="0" cellpadding="0" border="0" padding="15px">
+                        Accountability requires believing in yourself enough to be 100% dedicated to getting the work done.
+                        Most people fail to take an idea to fruition because the un  expected challenges become more than they think they can handle and thus they no longer want to be accountable.
+                        They lose the belief in themselves to see things through all the way to the end.
+                      </td>
+                    </tr>
+                  </table>
+
+
+                </td>
+              </tr>
+
+
+            </table>
+
+          </td>
+        </tr>
+      </table>
+
+    </body>
+</html>
